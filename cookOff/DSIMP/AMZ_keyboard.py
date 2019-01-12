2a5b2c7923cc1daed464de6d856df067ef9cd786
Rahul Joshi <35663367+rjoshi47@users.noreply.github.com>
2019-01-12 19:22:17 +0530

    Update AMZ_keyboard.py

--- cookOff/DSIMP/AMZ_keyboard.py
+++ cookOff/DSIMP/AMZ_keyboard.py
@@ -1,36 +1,22 @@
 '''
 Created on May 10, 2018
-
+https://practice.geeksforgeeks.org/problems/special-keyboard/0
 @author: devuser
 '''
 
-def getMaxAs(n, cv, vPress):
-    print((n, cv, vPress))
+def getMaxAs(n, cv, clipBoard):
+    print((n, cv, clipBoard))
     if n == 0:
         return cv
-    if vPress != 0:
-        return getMaxAs(n-1, cv+1, vPress)
+    if vPress != 0: # 
+        return getMaxAs(n-1, cv+1, clipBoard)
     elif vPress == 0:
         if n >= 3:
-            return max(getMaxAs(n-1, cv+1, vPress), getMaxAs(n-3, 2*cv, cv))
+            return max(getMaxAs(n-1, cv+1, clipBoard), getMaxAs(n-3, 2*cv, cv))
         else:
             return n+cv
     
-    '''
-    if n == 0:
-        return cv
-    if n > 3:
-        if vPress == 0:
-            return max(getMaxAs(n-1, cv+1, vPress), getMaxAs(n-3, 2*cv, cv))
-        else:
-            return getMaxAs(n-1, cv + vPress, vPress)
-            #return max(getMaxAs(n-1, cv+1, vPress), getMaxAs(n-1, cv + 2*vPress, vPress))
-    else:
-        if vPress != 0:
-            return getMaxAs(n-1, cv + vPress, vPress)
-        else:
-            return (n + cv)
-    '''
+
 res = []
 for _ in range(int(input())):
     n = int(input())
